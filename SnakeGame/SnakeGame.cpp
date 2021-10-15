ebf2fbc2ae6e428ac7c586f38a32e58f0ca8179c
Musa17 <amusul00@fesb.hr>
2021-10-15 14:18:47 +0200

    Adding player's controls

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -77,7 +77,27 @@ void Draw()
 
 void Input()
 {
-
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
 }
 
 void Logic()
