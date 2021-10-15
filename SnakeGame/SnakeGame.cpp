5ac4f8bfa212d2320b36b5be0ed4698441365df5
Musa17 <amusul00@fesb.hr>
2021-10-15 14:48:48 +0200

    Adding quit game  when player eat himself

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -168,6 +168,11 @@ void Logic()
 	//if (x > width || x < 0 || y > height || y < 0)
 	//  gameOver = true;
 
+	// Game Over when player eat himself
+	for (int i = 0; i < nTail; i++)
+		if (tailX[i] == x && tailY[i] == y)
+			gameOver = true;
+
 	// Eating fruit
 	if (x == fruitX && y == fruitY)
 	{
